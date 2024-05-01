8b3f1a65f2e55e560d6e947199935c2450126852
Alan Edwardes <alan@alanedwardes.com>
2024-05-01 23:33:51 +0100

    Adds user agent filtering, tweaks to path patterns.

--- spam-filter/index.js
+++ spam-filter/index.js
@@ -1,17 +1,23 @@
 function handler(event) {
     // Prefix filters to apply to the path - for example https://example.com/admin*
-    var prefixes = ["/cms", "/portal", "/admin", "/backoffice", "/downloader", "/vendor", "/contrib", "/orchard", "/cubemail", "/confluence"];
+    var prefixes = ["/cms", "/portal", "/admin", "/backoffice", "/downloader", "/vendor", "/contrib", "/orchard", "/cubemail", "/confluence", "/index.action"];
     
     // Less-specific matches anywhere in the path - for example */phpmyadmin/*
-    var matches = ["/.env", "/cgi-bin/", "/phpmyadmin/", "/sqlmanager/", "/.git/", "/.well-known/", "/wordpress/", "/wp-", "/wp/", "/wp1/", "/wp2/", "/rwservlet/", "/zb_users/", "/rdweb/", "/fckeditor/", "/nmaplowercheck", "/.vscode/", "/.vs/", "/alfa_data/"];
+    var matches = ["/.env", "/cgi-bin/", "/phpmyadmin/", "/sqlmanager/", "/.git/", "/.well-known/", "/wordpress/", "/wp-", "/wp/", "/wp1/", "/wp2/", "/zb_users/", "/rdweb/", "/fckeditor/", "/nmaplowercheck", "/.vscode/", "/.vs/", "/alfa_data/"];
     
     // Suffix matches to apply to the path - for example *.php
-    var suffixes = [".php", ".php7", ".cgi", ".pl", ".cfm", ".jhtml", ".shtml", ".jsa", ".jsp", ".asp", ".aspx", ".ashx", ".ds_store", "sftp.json", "sftp-config.json"];
+    var suffixes = [".php", ".php7", ".cgi", ".pl", ".cfm", ".jhtml", ".shtml", ".jsa", ".jsp", ".asp", ".aspx", ".ashx", ".do", ".ds_store", "sftp.json", "sftp-config.json", "/rwservlet"];
+
+    // Partial user agent matches
+    var userAgents = ["censysinspect", "go-http-client"];
 
     var uri = event.request.uri.toLowerCase();
+    var agent = (((event.request.headers['user-agent'] ?? {})['value']) ?? '').toLowerCase();
+    
     if (prefixes.some(prefix => uri.startsWith(prefix)) ||
         matches.some(match => uri.match(match)) ||
-        suffixes.some(suffix => uri.endsWith(suffix))) {
+        suffixes.some(suffix => uri.endsWith(suffix)) ||
+        userAgents.some(match => agent.match(match))) {
         return { statusCode: 402, statusDescription: 'Payment Required' };
     }
     
