0f726052d4d6821754e614ceb56b55935cc36354
Alan Edwardes <alan@alanedwardes.com>
2024-06-03 21:07:54 +0100

    Removes portal and admin from disallow list (too generic), adds .pem and "palo alto networks" to blocklists.

--- spam-filter/index.js
+++ spam-filter/index.js
@@ -1,15 +1,15 @@
 function handler(event) {
     // Prefix filters to apply to the path - for example https://example.com/admin*
-    var prefixes = ["/cms", "/portal", "/admin", "/backoffice", "/downloader", "/vendor", "/contrib", "/orchard", "/cubemail", "/confluence"];
+    var prefixes = ["/cms", "/backoffice", "/downloader", "/vendor", "/contrib", "/orchard", "/cubemail", "/confluence"];
     
     // Less-specific matches anywhere in the path - for example */phpmyadmin/*
     var matches = ["/.env", "/cgi-bin/", "/phpmyadmin/", "/sqlmanager/", "/.git/", "/.well-known/", "/wordpress/", "/wp-", "/wp/", "/wp1/", "/wp2/", "/zb_users/", "/rdweb/", "/fckeditor/", "/nmaplowercheck", "/.vscode/", "/.vs/", "/alfa_data/"];
     
     // Suffix matches to apply to the path - for example *.php
-    var suffixes = [".php", ".php7", ".cgi", ".pl", ".cfm", ".jhtml", ".shtml", ".jsa", ".hsp", ".jsp", ".asp", ".aspx", ".ashx", ".do", ".action", ".ds_store", ".alfa", "sftp.json", "sftp-config.json", "/rwservlet"];
+    var suffixes = [".php", ".php7", ".cgi", ".pl", ".cfm", ".jhtml", ".shtml", ".jsa", ".hsp", ".jsp", ".asp", ".aspx", ".ashx", ".do", ".action", ".ds_store", ".alfa", "sftp.json", "sftp-config.json", "/rwservlet", ".pem"];
 
     // Partial user agent matches
-    var userAgents = ["censysinspect", "go-http-client"];
+    var userAgents = ["censysinspect", "go-http-client", "palo alto networks"];
 
     var uri = event.request.uri.toLowerCase();
     var agent = (((event.request.headers['user-agent'] ?? {})['value']) ?? '').toLowerCase();
